7c5a68b1e85775aefcd6ee0ef41c92054329edd0
Steven Pilato <stevenpilato@Stevens-MacBook-Air.local>
2026-03-27 10:23:43 -0400

    modified binomial block assignment with prob and ntrial params and created new trial fomrat csv

--- rcode/binomial_block_assignment_simulation.R
+++ rcode/binomial_block_assignment_simulation.R
@@ -9,12 +9,15 @@ set.seed(1234)
 res <- list()
 library(slider)
 library(purrr)
+ntrials <- 60
+p_unexpected <- .2
+trial_total <- ntrials*p_unexpected
 unexpected_combinations <- safely(function(x){
-  trials_categories <- rbinom(100, 1, .15)
+  trials_categories <- rbinom(ntrials, 1, p_unexpected)
   total_unexp <- sum(trials_categories) 
   moving_sum <- slide_sum(trials_categories, before = 1, step = 1)
-  if(total_unexp < 15 | total_unexp > 15){
-    print('P > .15 or P < .15')
+  if(total_unexp < trial_total | total_unexp > trial_total){
+    print('P > .2 or P < .2')
     return(NULL)
   } else{
     if(sum(moving_sum==2)){
@@ -26,7 +29,8 @@ unexpected_combinations <- safely(function(x){
   }
 })
 
-res <- map(1:50000, .f = unexpected_combinations)
+nruns <- 100000
+res <- map(1:nruns, .f = unexpected_combinations)
 mathing_trials <- list()
 index <- 1
 for(i in res){
@@ -43,6 +47,10 @@ final_df <- do.call(cbind, mathing_trials)
 
 final_df_no_dups <- t(final_df[!duplicated(final_df),])
 final_df_no_dups <- as.data.frame(final_df_no_dups[final_df_no_dups[, 1] != 1,])
+
+nrow(final_df_no_dups)
+rowSums(final_df_no_dups)
+
 readr::write_csv(x = final_df_no_dups, file = '.csv')
 
 
